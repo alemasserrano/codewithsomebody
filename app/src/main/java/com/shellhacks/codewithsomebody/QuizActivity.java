acf41f0bc3e093d48d1446af589b769b296624ac
Fernando Serrano <fserr009@fiu.edu>
2019-09-22 05:28:22 -0400

    add func to next in quiz

--- app/src/main/java/com/shellhacks/codewithsomebody/QuizActivity.java
+++ app/src/main/java/com/shellhacks/codewithsomebody/QuizActivity.java
@@ -2,7 +2,12 @@ package com.shellhacks.codewithsomebody;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.shellhacks.codewithsomebody.ui.login.LoginActivity;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -10,7 +15,13 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        final Button nextButton = findViewById(R.id.next);
 
-
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuizActivity.this, Calendar.class));
+            }
+        });
     }
 }
