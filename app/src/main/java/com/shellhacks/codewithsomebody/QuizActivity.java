87687062eba6b55e11fa0d979a387bbd6380e7ce
Fernando Serrano <fserr009@fiu.edu>
2019-09-22 02:41:37 -0400

    added redirect for register

--- app/src/main/java/com/shellhacks/codewithsomebody/QuizActivity.java
+++ app/src/main/java/com/shellhacks/codewithsomebody/QuizActivity.java
@@ -3,8 +3,6 @@ package com.shellhacks.codewithsomebody;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -13,12 +11,6 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        final Button loginButton = findViewById(R.id.register);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-
-            }
-        });
 
     }
 }
--- app/src/main/java/com/shellhacks/codewithsomebody/ui/login/LoginActivity.java
+++ app/src/main/java/com/shellhacks/codewithsomebody/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.shellhacks.codewithsomebody.QuizActivity;
 import com.shellhacks.codewithsomebody.R;
 import com.shellhacks.codewithsomebody.ui.login.LoginViewModel;
 import com.shellhacks.codewithsomebody.ui.login.LoginViewModelFactory;
@@ -40,6 +42,7 @@ public class LoginActivity extends AppCompatActivity {
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
+        final Button registerButton = findViewById(R.id.register);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
@@ -115,6 +118,13 @@ public class LoginActivity extends AppCompatActivity {
                 loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, QuizActivity.class));
             }
         });
     }
